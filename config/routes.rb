fcef6b2a73d902bb006cf9fceb42b41c439a8953
groteck <groteck@gmail.com>
2011-10-08 17:37:34 +0100

    webs estaticas añadidas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SampleApp::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,7 +53,7 @@ SampleApp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-
+  root :to => 'pages#home'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
