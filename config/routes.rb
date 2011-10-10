c364bf768fdb49498d0d16a1c2c6046f877c6632
groteck <groteck@gmail.com>
2011-10-10 11:18:56 +0100

    helper añadido

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ SampleApp::Application.routes.draw do
  
   #agregado para pasar el test rspec
   get "pages/about"
+  get "pages/help"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
